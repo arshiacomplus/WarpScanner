f48b697da1002a22a15b7eea97629533ddd245de
arshiacomplus <163334604+arshiacomplus@users.noreply.github.com>
2024-05-23 17:42:36 +0330

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,1 +1,14 @@
 
+#!/bin/bash
+command -v python3 &>/dev/null || {
+    echo "Python 3 نصب نیست. در حال نصب..."
+
+    pkg install python-pip 
+    pkg install git python
+}
+    # این دستور بستگی به سیستم عامل شما دارد. مثال
+# دانلود اسکریپت پایتون
+curl -fsSL -o script.py https://github.com/arshiacomplus/Test/blob/main/FixResult.py
+
+# اجرای اسکریپت پایتون
+python3 script.py
