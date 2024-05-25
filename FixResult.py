6f18fb743bdf48519702305aaa607491f02bec07
arshiacomplus <163334604+arshiacomplus@users.noreply.github.com>
2024-05-25 13:11:57 +0330

    Update FixResult.py

--- FixResult.py
+++ FixResult.py
@@ -1,3 +1,6 @@
+import requests
+import re
+import os
 import socket
 from concurrent.futures import ThreadPoolExecutor
 import time
@@ -18,6 +21,24 @@ def scan_ip_port(ip, port):
                 results.append((ip, port, elapsed_time))
             else:
                 print(f"IP: {ip} Port: {port} is not responding or closed.")
+            
+                
+    except Exception as e:
+        print(f"Error scanning {ip}:{port} - {e}")
+
+
+def scan_ip_port2(ip, port):
+    
+    try:
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+            s.settimeout(0.5)
+            start_time = time.time()
+            result = s.connect_ex((ip, port))
+            end_time = time.time()
+            
+            if result == 0:
+                elapsed_time = (end_time - start_time) * 1000
+                results.append((ip, port, elapsed_time))
             
                 
     except Exception as e:
@@ -72,9 +93,179 @@ def main():
     
     if sorted_results:
         best_result = sorted_results[0]
+        print("\033[91m")
         print(f"""
-        nBest ping: IP:  {best_result[0]}:{best_result[1]}
+        Best ping: IP:  {best_result[0]}:{best_result[1]}
         Ping: {best_result[2]:.2f} ms""")
 
+def main2():
+    
+    print('please wait (scaning ip)      ...........')
+    def free_cloudflare_account():
+            
+        print('''
+        
+please wait (make wireguard)      ...........
+            
+            ''')
+       
+        response = requests.get("https://api.zeroteam.top/warp?format=sing-box")
+        output = response.text
+
+        
+        public_key_pattern = r'"2606:4700:[0-9a-f:]+/128"'
+        private_key_pattern = r'"private_key":"[0-9a-zA-Z/+]+="'
+        reserved_pattern = r'"reserved":[[0-9]+(,[0-9]+){2}]'
+
+        public_key_search = re.search(public_key_pattern, output)
+        private_key_search = re.search(private_key_pattern, output)
+        reserved_search = re.search(reserved_pattern, output)
+
+      
+        public_key = public_key_search.group(0).replace('"', '') if public_key_search else None
+        private_key = private_key_search.group(0).split(':')[1].replace('"', '') if private_key_search else None
+        reserved = reserved_search.group(0).replace('"reserved":', '').replace('"', '') if reserved_search else None
+        os.system('clear')
+        
+        print(f'''
+{{
+        "route": {{                                                         "geoip": {{
+                "path": "geo-assets\\\\sagernet-sing-geoip-geoip.db"
+                }},                                                         "geosite": {{
+                "path": "geo-assets\\\\sagernet-sing-geosite-geosite.db"
+                }},                                                         "rules": [
+                {{                                                                  "inbound": "dns-in",
+                        "outbound": "dns-out"                              }},
+                {{                                                                  "port": 53,
+                        "outbound": "dns-out"                              }},
+                {{                                                                  "clash_mode": "Direct",
+                        "outbound": "direct"                               }},
+                {{                                                                  "clash_mode": "Global",
+                        "outbound": "select"
+                }}
+                ],
+                "auto_detect_interface": true,
+                "override_android_vpn": true                       }},
+        "outbounds": [
+                {{
+                "type": "selector",
+                "tag": "select",                                           "outbounds": [
+                        "auto",
+                        "IP->Iran, Telegram: @arshiacomplus",
+                        "IP->Main, Telegram: @arshiacomplus"
+                ],
+                "default": "auto"
+                }},
+                {{
+                "type": "urltest",
+                "tag": "auto",
+                "outbounds": [
+                        "IP->Iran, Telegram: @arshiacomplus",
+                        "IP->Main, Telegram: @arshiacomplus"
+                ],
+                "url": "http://cp.cloudflare.com/",
+                "interval": "10m0s"
+                }},
+                {{
+                "type": "wireguard",
+                "tag": "IP->Iran, Telegram: @arshiacomplus",
+                "local_address": [
+                        "172.16.0.2/32",
+                        "{public_key}"
+                ],
+                "private_key": "{private_key}",
+                "server": "{best_result[0]}",
+                "server_port": {best_result[1]},
+                "peer_public_key": "bmXOC+F1FxEMF9dyiK2H5/1SUtzH0JuVo51h2wPfgyo=",
+                "reserved": {reserved},
+                "mtu": 1280,
+                "fake_packets": "5-10"
+                }},
+                {{
+                "type": "wireguard",
+                "tag": "IP->Main, Telegram: @arshiacomplus",
+                "detour": "IP->Iran, Telegram: @arshiacomplus",
+                "local_address": [
+                        "172.16.0.2/32",
+                        "{public_key}"
+                ],
+                "private_key": "{private_key}",
+                "server": "{best_result[0]}",
+                "server_port": {best_result[1]},
+                "peer_public_key": "bmXOC+F1FxEMF9dyiK2H5/1SUtzH0JuVo51h2wPfgyo=",
+                "reserved": {reserved},
+                "mtu": 1280,
+                "fake_packets": "5-10"
+                }},
+                {{
+                "type": "dns",
+                "tag": "dns-out"
+                }},
+                {{
+                "type": "direct",
+                "tag": "direct"
+                }},
+                {{
+                "type": "direct",
+                "tag": "bypass"
+                }},
+                {{
+                "type": "block",
+                "tag": "block"
+                }}
+        ]
+        }}
+''')
+
+
+    start_ip = ["188.114.96.0", "162.159.192.0","162.159.195.0"]
+    end_ip = ["188.114.98.224", "162.159.193.224","162.159.195.224"]
+    ports = [1074 , 894, 908]
+    
+    ip_range = create_ip_range(start_ip[0], end_ip[0])
+    
+    ip_range2 = create_ip_range(start_ip[1], end_ip[1])
+
+    ip_range3 = create_ip_range(start_ip[2], end_ip[2])
+    
+    with ThreadPoolExecutor(max_workers=100) as executor:
+        for ip in ip_range:
+            for port in ports:
+                executor.submit(scan_ip_port2, ip, port)
+    with ThreadPoolExecutor(max_workers=100) as executor:
+        for ip in ip_range2:
+            for port in ports:
+                executor.submit(scan_ip_port2, ip, port)
+    with ThreadPoolExecutor(max_workers=100) as executor:
+        for ip in ip_range3:
+            for port in ports:
+                executor.submit(scan_ip_port2, ip, port)
+    executor.shutdown(wait=True)
+
+    sorted_results = sorted(results, key=lambda x: x[2])
+
+    
+
+    
+    if sorted_results:
+        best_result = sorted_results[0]
+        print(f"""
+ Best ping: IP:  {best_result[0]}:{best_result[1]}
+ Ping: {best_result[2]:.2f} mbs""")
+    free_cloudflare_account()
+
 if __name__ == "__main__":
-    main()
+    os.system('clear')
+    what=input("""scan ip (enter 1)
+wireguard config(enter 2)
+
+>:""")
+    while what!='1' and what !='2':
+    	what=input("""scan ip (enter 1)
+wireguard config(enter 2)
+
+>:""")
+    if what =='1':
+        main()
+    elif what=='2':
+        main2()
