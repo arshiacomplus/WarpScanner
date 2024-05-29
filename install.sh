a329a72006dd7219476c6526d6e66e758a565479
arshiacomplus <163334604+arshiacomplus@users.noreply.github.com>
2024-05-30 01:01:34 +0330

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,12 +3,12 @@
 # دانلود فایل پایتون
 curl -fsSL -o FixResult.py https://raw.githubusercontent.com/arshiacomplus/Test/main/FixResult.py
 
-command -v python3 &>/dev/null || {
+command -v python &>/dev/null || {
     echo "wait for install python3...."
 
-    pkg update && pkg upgrade
+    
     pkg install python-pip 
     pkg install git python
 }
 # اجرای فایل پایتون
-python3 FixResult.py
+python FixResult.py
