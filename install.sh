6d6a315da3f5ce72c97f3498519d8d072beae0db
arshiacomplus <163334604+arshiacomplus@users.noreply.github.com>
2024-06-05 18:37:37 +0330

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,49 +3,53 @@
 termux-setup-storage
 
 update_packages() {
-    echo "Updating packages..."
-    pkg update -y || { echo "Failed to update packages. Exiting."; exit 1; }
+    echo "Updating packages..."
+    pkg update -y || { echo "Failed to update packages. Exiting."; exit 1; }
 }
 
 install_python() {
-    echo "Installing Python..."
+    echo "Installing Python..."
     pkg install python -y || { echo "Failed to install Python. Exiting."; exit 1; }
-    pkg install python-pip -y || { echo "Failed to install Python. Exiting."; exit 1; }
+    pkg install python-pip -y || { echo "Failed to install Python. Exiting."; exit 1; }
 }
 
 install_git() {
-    echo "Installing Git..."
-    pkg install git || { echo "Failed to install Git. Exiting."; exit 1; }
-    pkg install git python -y || { echo "Failed Failed to install git python. Exiting."; exit 1; }
-
+    echo "Installing Git..."
+    pkg install git || { echo "Failed to install Git. Exiting."; exit 1; }
+    pkg install python git -y || { echo "Failed to install Git Python. Exiting."; exit 1; }
 }
 
 install_wget() {
-    echo "Installing wget..."
-    pkg install wget || { echo "Failed to install wget. Exiting."; exit 1; }
+    echo "Installing wget..."
+    pkg install wget || { echo "Failed to install wget. Exiting."; exit 1; }
 }
 
 install_curl() {
-    echo "Installing curl..."
-    pkg install curl || { echo "Failed to install curl. Exiting."; exit 1; }
+    echo "Installing curl..."
+    pkg install curl || { echo "Failed to install curl. Exiting."; exit 1; }
 }
 
 update_packages
 
-if ! command -v python &>/dev/null; then
-    install_python
+if ! command -v python || ! command -v pip; then
+    install_python
 fi
 
-if ! command -v git &>/dev/null; then
-    install_git
+if ! command -v git; then
+    install_git
 fi
 
-if ! command -v wget &>/dev/null; then
-    install_wget
+if ! command -v wget; then
+    install_wget
 fi
 
-if ! command -v curl &>/dev/null; then
-    install_curl
+if ! command -v curl; then
+    install_curl
+fi
+
+if [ -f FixResult.py ]; then
+    rm FixResult.py
+    echo "Updating FixResult.py..."
 fi
 
 curl -fsSL -o FixResult.py https://raw.githubusercontent.com/arshiacomplus/Test/main/FixResult.py || { echo "Failed to download FixResult.py. Exiting."; exit 1; }
