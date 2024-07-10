7b90c8b99fbe2fb297bddb62bda05e7018198e94
Arshia <163334604+arshiacomplus@users.noreply.github.com>
2024-07-10 12:43:34 +0330

    Update install.sh

--- install.sh
+++ install.sh
@@ -54,7 +54,7 @@ if [ -f WarpScanner.py ]; then
 fi
 if [ -f WarpScanner.py ]; then
     first_line=$(head -n 1 WarpScanner.py)
-    if [ "$first_line" != "V=9" ]; then
+    if [ "$first_line" != "V=10" ]; then
         rm WarpScanner.py
         echo "Updating WarpScanner.py..."
     else
